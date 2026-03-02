7a2835dfc7427ce733289be6c741458886866999
mozhaa <mozhay2005@gmail.com>
2026-03-02 16:18:41 +0300

    fix: add new node to __init__

--- __init__.py
+++ __init__.py
@@ -1,12 +1,19 @@
-from .nodes import LoadImageFromFolder, LoadImagesFromFolder, SaveImageWithPrompt
+from .nodes import (
+    LoadImageFromFolder,
+    LoadImagesFromFolder,
+    LoadImageWithPrompt,
+    SaveImageWithPrompt,
+)
 
 NODE_CLASS_MAPPINGS = {
     "Save Image With Prompt": SaveImageWithPrompt,
     "Load Image From Folder": LoadImageFromFolder,
     "Load Images From Folder": LoadImagesFromFolder,
+    "Load Image With Prompt": LoadImageWithPrompt,
 }
 NODE_DISPLAY_NAME_MAPPINGS = {
     "Save Image With Prompt": "Save Image With Prompt",
     "Load Image From Folder": "Load Image From Folder",
     "Load Images From Folder": "Load Images From Folder",
+    "Load Image With Prompt": "Load Image With Prompt",
 }
